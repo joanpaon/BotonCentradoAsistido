3e360c413d6144d9045affd0fa844591058e2a04
joanpaon <joanpaon@gmail.com>
2019-04-22 20:18:47 +0200

    :sparkles: Código optimizado

--- src/org/japo/java/forms/GUI.java
+++ src/org/japo/java/forms/GUI.java
@@ -26,29 +26,11 @@ import org.japo.java.libraries.UtilesSwing;
  */
 public final class GUI extends JFrame {
 
-// Propiedades App
-    public static final String PRP_FAVICON_RESOURCE = "favicon_resource";
-    public static final String PRP_FONT_RESOURCE = "font_resource";
-    public static final String PRP_FORM_TITLE = "form_title";
-    public static final String PRP_FORM_WIDTH = "form_width";
-    public static final String PRP_FORM_HEIGHT = "form_height";
-    public static final String PRP_IMAGE_RESOURCE = "image_resource";
-    public static final String PRP_LOOK_AND_FEEL_PROFILE = "look_and_feel_profile";
-
-    // Valores por Defecto
-    public static final String DEF_FAVICON_RESOURCE = "img/favicon.png";
-    public static final String DEF_FONT_FALLBACK_NAME = Font.SERIF;
-    public static final String DEF_FONT_SYSTEM_NAME = "Kaufmann BT";
-    public static final String DEF_FORM_TITLE = "Swing Manual App";
-    public static final int DEF_FORM_WIDTH = 500;
-    public static final int DEF_FORM_HEIGHT = 300;
-    public static final String DEF_LOOK_AND_FEEL_PROFILE = UtilesSwing.LNF_WINDOWS_PROFILE;
-
     // Referencias
     private final Properties prp;
 
     // Fuentes
-    private Font fntSample;
+    private Font fntBoton;
 
     // Constructor
     public GUI(Properties prp) {
@@ -102,33 +84,25 @@ public final class GUI extends JFrame {
     // Inicialización Anterior    
     private void initBefore() {
         // Establecer LnF
-        UtilesSwing.establecerLnFProfile(prp.getProperty(
-                PRP_LOOK_AND_FEEL_PROFILE, DEF_LOOK_AND_FEEL_PROFILE));
+        UtilesSwing.establecerLnFProfile(prp.getProperty("look_and_feel_profile"));
 
         // Fuentes
-        fntSample = UtilesSwing.generarFuenteRecurso(
-                prp.getProperty(PRP_FONT_RESOURCE),
-                DEF_FONT_SYSTEM_NAME,
-                DEF_FONT_FALLBACK_NAME);
+        fntBoton = UtilesSwing.generarFuenteRecurso(prp.getProperty("font_resource"));
     }
 
     // Inicialización Posterior
     private void initAfter() {
         // Establecer Favicon
-        UtilesSwing.establecerFavicon(this, prp.getProperty(
-                PRP_FAVICON_RESOURCE, DEF_FAVICON_RESOURCE));
+        UtilesSwing.establecerFavicon(this, prp.getProperty("img_favicon_resource"));
 
         // Botón
-        btnTerminar.setFont(fntSample.deriveFont(Font.BOLD, 40f));
+        btnTerminar.setFont(fntBoton.deriveFont(Font.BOLD, 40f));
 
-        // Ventana Principal
-        setTitle(prp.getProperty(PRP_FORM_TITLE, DEF_FORM_TITLE));
-        try {
-            int height = Integer.parseInt(prp.getProperty(PRP_FORM_HEIGHT));
-            int width = Integer.parseInt(prp.getProperty(PRP_FORM_WIDTH));
-            setSize(width, height);
-        } catch (NumberFormatException e) {
-            setSize(DEF_FORM_WIDTH, DEF_FORM_HEIGHT);
-        }
+        // Ventana Principal - Propiedades
+        setTitle(prp.getProperty("form_title"));
+        int width = Integer.parseInt(prp.getProperty("form_width"));
+        int height = Integer.parseInt(prp.getProperty("form_height"));
+        setSize(width, height);
+        setLocationRelativeTo(null);
     }
 }
